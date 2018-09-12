fcf13dc2109f563c53c8c757e902778b643c624b
Monica  Karasek <mlkarasek@gmail.com>
2018-09-12 23:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-class Driver(name, startDate) {
+class Driver {
+constructor(name, startDate) {
  this.name = name
- this.startDate = startDate
+ this.startDate = new Date(startDate)
 }
+yearsExperienceFromBeginningOf(year) {
+     let endDate = new Date(year, 0, 1)
+     let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+     return parseInt(totalYears)
+   }
+ }
+
+ class Route {
+   
+ }
